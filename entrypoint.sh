260aaf9c83c6e2816eed18ce7af724dfb7a2efc7
ofer-legit <104299267+ofer-legit@users.noreply.github.com>
2023-01-31 15:24:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG="${PWD}/kubeconfig"
+echo "${VPN_CLIENT_CONFIG}" | base64 -d > openvpn.ovpn
 
 echo "running entrypoint command(s)"
 
